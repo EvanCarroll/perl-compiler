5e4370851214957331bcd04e78ea686a2b486b12
Todd Rinaldo <toddr@cpan.org>
2017-06-19 14:50:08 -0500

    Tidy perl_parse.h for further re-factor

--- perl_parse.h
+++ perl_parse.h
@@ -25,12 +25,12 @@ bc_perl_parse(pTHXx_ XSINIT_t xsinit, int argc, char **argv, char **env)
 	 * the area we are able to modify is limited to the size of
 	 * the original argv[0].  (See below for 'contiguous', though.)
 	 * --jhi */
-	 const char *s = NULL;
-	 int i;
-	 const UV mask = ~(UV)(PTRSIZE-1);
-         /* Do the mask check only if the args seem like aligned. */
-	 const UV aligned =
-	   (mask < ~(UV)0) && ((PTR2UV(argv[0]) & mask) == PTR2UV(argv[0]));
+	const char *s = NULL;
+	int i;
+	const UV mask = ~(UV)(PTRSIZE-1);
+
+    /* Do the mask check only if the args seem like aligned. */
+	const UV aligned = (mask < ~(UV)0) && ((PTR2UV(argv[0]) & mask) == PTR2UV(argv[0]));
 
 	 /* See if all the arguments are contiguous in memory.  Note
 	  * that 'contiguous' is a loose term because some platforms
@@ -62,38 +62,24 @@ bc_perl_parse(pTHXx_ XSINIT_t xsinit, int argc, char **argv, char **env)
 	 }
 
 	 /* Can we grab env area too to be used as the area for $0? */
-	 if (s && PL_origenviron && !PL_use_safe_putenv) {
-	      if ((PL_origenviron[0] == s + 1)
-		  ||
-		  (aligned &&
-		   (PL_origenviron[0] >  s &&
-		    PL_origenviron[0] <=
-		    INT2PTR(char *, PTR2UV(s + PTRSIZE) & mask)))
-		 )
-	      {
-		   s = PL_origenviron[0];
-		   while (*s) s++;
-		   my_setenv("NoNe  SuCh", NULL);
-		   /* Force copy of environment. */
-		   for (i = 1; PL_origenviron[i]; i++) {
-			if (PL_origenviron[i] == s + 1
-			    ||
-			    (aligned &&
-			     (PL_origenviron[i] >  s &&
-			      PL_origenviron[i] <=
-			      INT2PTR(char *, PTR2UV(s + PTRSIZE) & mask)))
-			   )
-			{
-			     s = PL_origenviron[i];
-			     while (*s) s++;
-			}
-			else
-			     break;
-		   }
-	      }
-	 }
+	if (s && PL_origenviron && !PL_use_safe_putenv) {
+        if ((PL_origenviron[0] == s + 1) || (aligned && (PL_origenviron[0] > s && PL_origenviron[0] <= INT2PTR(char *, PTR2UV(s + PTRSIZE) & mask))) ) {
+            s = PL_origenviron[0];
+            while (*s) s++;
+            my_setenv("NoNe  SuCh", NULL);
+            /* Force copy of environment. */
+            for (i = 1; PL_origenviron[i]; i++) {
+                if (PL_origenviron[i] == s + 1 || (aligned && (PL_origenviron[i] >  s && PL_origenviron[i] <= INT2PTR(char *, PTR2UV(s + PTRSIZE) & mask))) ) {
+                    s = PL_origenviron[i];
+                    while (*s) s++;
+                }
+                else
+                    break;
+            }
+        }
+	}
 
-	 PL_origalen = s ? s - PL_origargv[0] + 1 : 0;
+	PL_origalen = s ? s - PL_origargv[0] + 1 : 0;
     
     /* END: Determine how long $0 is allowed to be */
 
@@ -139,11 +125,11 @@ void bc_parse_body(char **env, XSINIT_t xsinit)
     if (!scriptname)
 	scriptname = argv[0];
     if (PL_e_script) {
-	argc++,argv--;
-	scriptname = BIT_BUCKET;	/* don't look for script or read stdin */
+        argc++,argv--;
+        scriptname = BIT_BUCKET;	/* don't look for script or read stdin */
     }
     else if (scriptname == NULL) {
-	scriptname = "-";
+        scriptname = "-";
     }
 
     /* init_perllib(); We hard code @INC on our own.*/
@@ -169,9 +155,10 @@ void bc_parse_body(char **env, XSINIT_t xsinit)
     boot_core_mro();
 
     if (xsinit)
-	(*xsinit)(aTHX);	/* in case linked C routines want magical variables */
+        (*xsinit)(aTHX);	/* in case linked C routines want magical variables */
 
     init_predump_symbols();
+
     /* init_postdump_symbols not currently designed to be called */
     /* more than once (ENV isn't cleared first, for example)	 */
     /* But running with -u leaves %ENV & @ARGV undefined!    XXX */
@@ -184,53 +171,48 @@ void bc_parse_body(char **env, XSINIT_t xsinit)
      * look like the user wants to use UTF-8. */
     if (PL_unicode) {
 	 /* Requires init_predump_symbols(). */
-	 if (!(PL_unicode & PERL_UNICODE_LOCALE_FLAG) || PL_utf8locale) {
-	      IO* io;
-	      PerlIO* fp;
-	      SV* sv;
+        if (!(PL_unicode & PERL_UNICODE_LOCALE_FLAG) || PL_utf8locale) {
+            IO* io;
+            PerlIO* fp;
+            SV* sv;
+  
+            /* Turn on UTF-8-ness on STDIN, STDOUT, STDERR
+             * and the default open disciplines. */
+            if ((PL_unicode & PERL_UNICODE_STDIN_FLAG) && PL_stdingv  && (io = GvIO(PL_stdingv)) && (fp = IoIFP(io)))
+                PerlIO_binmode(aTHX_ fp, IoTYPE(io), 0, ":utf8");
+            
+            if ((PL_unicode & PERL_UNICODE_STDOUT_FLAG) && PL_defoutgv && (io = GvIO(PL_defoutgv)) && (fp = IoOFP(io)))
+                PerlIO_binmode(aTHX_ fp, IoTYPE(io), 0, ":utf8");
 
-	      /* Turn on UTF-8-ness on STDIN, STDOUT, STDERR
-	       * and the default open disciplines. */
-	      if ((PL_unicode & PERL_UNICODE_STDIN_FLAG) &&
-		  PL_stdingv  && (io = GvIO(PL_stdingv)) &&
-		  (fp = IoIFP(io)))
-		   PerlIO_binmode(aTHX_ fp, IoTYPE(io), 0, ":utf8");
-	      if ((PL_unicode & PERL_UNICODE_STDOUT_FLAG) &&
-		  PL_defoutgv && (io = GvIO(PL_defoutgv)) &&
-		  (fp = IoOFP(io)))
-		   PerlIO_binmode(aTHX_ fp, IoTYPE(io), 0, ":utf8");
-	      if ((PL_unicode & PERL_UNICODE_STDERR_FLAG) &&
-		  PL_stderrgv && (io = GvIO(PL_stderrgv)) &&
-		  (fp = IoOFP(io)))
-		   PerlIO_binmode(aTHX_ fp, IoTYPE(io), 0, ":utf8");
-	      if ((PL_unicode & PERL_UNICODE_INOUT_FLAG) &&
-		  (sv = GvSV(gv_fetchpvs("\017PEN", GV_ADD|GV_NOTQUAL,
-					 SVt_PV)))) {
-		   U32 in  = PL_unicode & PERL_UNICODE_IN_FLAG;
-		   U32 out = PL_unicode & PERL_UNICODE_OUT_FLAG;
-		   if (in) {
-			if (out)
-			     sv_setpvs(sv, ":utf8\0:utf8");
-			else
-			     sv_setpvs(sv, ":utf8\0");
-		   }
-		   else if (out)
-			sv_setpvs(sv, "\0:utf8");
-		   SvSETMAGIC(sv);
-	      }
-	 }
+            if ((PL_unicode & PERL_UNICODE_STDERR_FLAG) && PL_stderrgv && (io = GvIO(PL_stderrgv)) && (fp = IoOFP(io)))
+                PerlIO_binmode(aTHX_ fp, IoTYPE(io), 0, ":utf8");
+
+            if ((PL_unicode & PERL_UNICODE_INOUT_FLAG) && (sv = GvSV(gv_fetchpvs("\017PEN", GV_ADD|GV_NOTQUAL, SVt_PV)))) {
+                U32 in  = PL_unicode & PERL_UNICODE_IN_FLAG;
+                U32 out = PL_unicode & PERL_UNICODE_OUT_FLAG;
+                if (in) {
+                    if (out)
+                        sv_setpvs(sv, ":utf8\0:utf8");
+                    else
+                        sv_setpvs(sv, ":utf8\0");
+                }
+                else if (out)
+                    sv_setpvs(sv, "\0:utf8");
+                    SvSETMAGIC(sv);
+            }
+        }
     }
 
     {
-	const char *s;
-    if ((s = PerlEnv_getenv("PERL_SIGNALS"))) {
-	 if (strEQ(s, "unsafe"))
-	      PL_signals |=  PERL_SIGNALS_UNSAFE_FLAG;
-	 else if (strEQ(s, "safe"))
-	      PL_signals &= ~PERL_SIGNALS_UNSAFE_FLAG;
-	 else
-	      Perl_croak(aTHX_ "PERL_SIGNALS illegal: \"%s\"", s);
-    }
+        const char *s;
+        if ((s = PerlEnv_getenv("PERL_SIGNALS"))) {
+            if (strEQ(s, "unsafe"))
+                 PL_signals |=  PERL_SIGNALS_UNSAFE_FLAG;
+            else if (strEQ(s, "safe"))
+                 PL_signals &= ~PERL_SIGNALS_UNSAFE_FLAG;
+            else
+                 Perl_croak(aTHX_ "PERL_SIGNALS illegal: \"%s\"", s);
+        }
     }
 
 
@@ -250,12 +232,12 @@ void bc_parse_body(char **env, XSINIT_t xsinit)
     }
 
     if (PL_do_undump)
-	my_unexec();
+        my_unexec();
 
     if (isWARN_ONCE) {
-	SAVECOPFILE(PL_curcop);
-	SAVECOPLINE(PL_curcop);
-	gv_check(PL_defstash);
+        SAVECOPFILE(PL_curcop);
+        SAVECOPLINE(PL_curcop);
+        gv_check(PL_defstash);
     }
 
     LEAVE;
@@ -270,106 +252,112 @@ STATIC void init_postdump_symbols(pTHX_ int argc, char **argv, char **env)
 {
     GV* tmpgv;
 
+    /* Set PL_toptarget to a PVIV set to "" */
     PL_toptarget = newSV_type(SVt_PVIV);
     sv_setpvs(PL_toptarget, "");
+
+    /* Set PL_bodytarget and PL_formtarget to a PVIV set to "" */
     PL_bodytarget = newSV_type(SVt_PVIV);
     sv_setpvs(PL_bodytarget, "");
     PL_formtarget = PL_bodytarget;
 
     TAINT;
 
+    /* @ARGV (PL_argvgv) is setup here. */
     init_argv_symbols(argc,argv);
 
     if ((tmpgv = gv_fetchpvs("0", GV_ADD|GV_NOTQUAL, SVt_PV))) {
-	sv_setpv(GvSV(tmpgv),PL_origfilename);
+    	sv_setpv(GvSV(tmpgv),PL_origfilename);
     }
+
     if ((PL_envgv = gv_fetchpvs("ENV", GV_ADD|GV_NOTQUAL, SVt_PVHV))) {
-	HV *hv;
-	bool env_is_not_environ;
-	SvREFCNT_inc_simple_void_NN(PL_envgv);
-	GvMULTI_on(PL_envgv);
-	hv = GvHVn(PL_envgv);
-	hv_magic(hv, NULL, PERL_MAGIC_env);
+    	HV *hv;
+    	bool env_is_not_environ;
+    	SvREFCNT_inc_simple_void_NN(PL_envgv);
+    	GvMULTI_on(PL_envgv);
+    	hv = GvHVn(PL_envgv);
+    	hv_magic(hv, NULL, PERL_MAGIC_env);
 
-	/* Note that if the supplied env parameter is actually a copy
-	   of the global environ then it may now point to free'd memory
-	   if the environment has been modified since. To avoid this
-	   problem we treat env==NULL as meaning 'use the default'
-	*/
-	if (!env)
-	    env = environ;
-	env_is_not_environ = env != environ;
-	if (env_is_not_environ
-	   )
-	{
-	    environ[0] = NULL;
-	}
-	if (env) {
-	  char *s, *old_var;
-          STRLEN nlen;
-	  SV *sv;
-          HV *dups = newHV();
+    	/* Note that if the supplied env parameter is actually a copy
+    	   of the global environ then it may now point to free'd memory
+    	   if the environment has been modified since. To avoid this
+    	   problem we treat env==NULL as meaning 'use the default'
+    	*/
+    	if (!env)
+    	    env = environ;
+    	env_is_not_environ = env != environ;
+    	if (env_is_not_environ) {
+            environ[0] = NULL;
+        }
+        if (env) {
+            char *s, *old_var;
+            STRLEN nlen;
+            SV *sv;
+            HV *dups = newHV();
 
-	  for (; *env; env++) {
-	    old_var = *env;
-
-	    if (!(s = strchr(old_var,'=')) || s == old_var)
-		continue;
-            nlen = s - old_var;
-
-            if (hv_exists(hv, old_var, nlen)) {
-                const char *name = savepvn(old_var, nlen);
-
-                /* make sure we use the same value as getenv(), otherwise code that
-                   uses getenv() (like setlocale()) might see a different value to %ENV
-                 */
-                sv = newSVpv(PerlEnv_getenv(name), 0);
-
-                /* keep a count of the dups of this name so we can de-dup environ later */
-                if (hv_exists(dups, name, nlen))
-                    ++SvIVX(*hv_fetch(dups, name, nlen, 0));
-                else
-                    (void)hv_store(dups, name, nlen, newSViv(1), 0);
-
-                Safefree(name);
+            for (; *env; env++) {
+                old_var = *env;
+    
+                if (!(s = strchr(old_var,'=')) || s == old_var)
+                    continue;
+                    nlen = s - old_var;
+    
+                    if (hv_exists(hv, old_var, nlen)) {
+                        const char *name = savepvn(old_var, nlen);
+    
+                        /* make sure we use the same value as getenv(), otherwise code that
+                           uses getenv() (like setlocale()) might see a different value to %ENV
+                         */
+                        sv = newSVpv(PerlEnv_getenv(name), 0);
+    
+                        /* keep a count of the dups of this name so we can de-dup environ later */
+                        if (hv_exists(dups, name, nlen))
+                            ++SvIVX(*hv_fetch(dups, name, nlen, 0));
+                        else
+                            (void)hv_store(dups, name, nlen, newSViv(1), 0);
+    
+                        Safefree(name);
+                    }
+                    else
+                        sv = newSVpv(s+1, 0);
+                (void)hv_store(hv, old_var, nlen, sv, 0);
+    
+                if (env_is_not_environ)
+                    mg_set(sv);
             }
-            else {
-                sv = newSVpv(s+1, 0);
+              
+            if (HvKEYS(dups)) {
+                /* environ has some duplicate definitions, remove them */
+                HE *entry;
+                hv_iterinit(dups);
+                while ((entry = hv_iternext_flags(dups, 0))) {
+                    STRLEN nlen;
+                    const char *name = HePV(entry, nlen);
+                    IV count = SvIV(HeVAL(entry));
+                    IV i;
+                    SV **valp = hv_fetch(hv, name, nlen, 0);
+    
+                    assert(valp);
+    
+                    /* try to remove any duplicate names, depending on the
+                       * implementation used in my_setenv() the iteration might
+                       * not be necessary, but let's be safe.
+                    */
+                    for (i = 0; i < count; ++i)
+                        my_setenv(name, 0);
+    
+                    /* and set it back to the value we set $ENV{name} to */
+                    my_setenv(name, SvPV_nolen(*valp));
+                }
             }
-	    (void)hv_store(hv, old_var, nlen, sv, 0);
-	    if (env_is_not_environ)
-	        mg_set(sv);
-	  }
-          if (HvKEYS(dups)) {
-              /* environ has some duplicate definitions, remove them */
-              HE *entry;
-              hv_iterinit(dups);
-              while ((entry = hv_iternext_flags(dups, 0))) {
-                  STRLEN nlen;
-                  const char *name = HePV(entry, nlen);
-                  IV count = SvIV(HeVAL(entry));
-                  IV i;
-                  SV **valp = hv_fetch(hv, name, nlen, 0);
 
-                  assert(valp);
-
-                  /* try to remove any duplicate names, depending on the
-                   * implementation used in my_setenv() the iteration might
-                   * not be necessary, but let's be safe.
-                   */
-                  for (i = 0; i < count; ++i)
-                      my_setenv(name, 0);
-
-                  /* and set it back to the value we set $ENV{name} to */
-                  my_setenv(name, SvPV_nolen(*valp));
-              }
-          }
-          SvREFCNT_dec_NN(dups);
-      }
+            SvREFCNT_dec_NN(dups);
+        }
     }
     TAINT_NOT;
 
     /* touch @F array to prevent spurious warnings 20020415 MJD */
+    /* B::C doesn't support perl -a so this is dead code. */
     if (PL_minus_a) {
       (void) get_av("main::F", GV_ADD | GV_ADDMULTI);
     }
