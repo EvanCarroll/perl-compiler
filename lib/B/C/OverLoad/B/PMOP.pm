c0a32d247e7a18ef2a2ca948bdfd86899a8f17f6
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2017-08-16 09:27:53 -0600

    deduplicate RegExp

--- lib/B/C/OverLoad/B/PMOP.pm
+++ lib/B/C/OverLoad/B/PMOP.pm
@@ -13,6 +13,8 @@ my ($swash_init);
 # FIXME really required ?
 sub PMf_ONCE() { 0x10000 };    # PMf_ONCE also not exported
 
+my %saved_re;
+
 sub do_save {
     my ($op) = @_;
 
@@ -78,9 +80,17 @@ sub do_save {
             $initpm->add('PL_hints |= HINT_RE_EVAL;');
         }
 
+        my $key = sprintf( "((%s, %s, SVs_TEMP|%s), 0x%x, 0x%x)", $qre, $relen, $utf8 ? 'SVf_UTF8' : '0', $pmflags, $op->reflags );
+
         # XXX Modification of a read-only value attempted. use DateTime - threaded
-        $initpm->sadd( "PM_SETRE(%s, CALLREGCOMP(newSVpvn_flags(%s, %s, SVs_TEMP|%s), 0x%x));", $sym, $qre, $relen, $utf8 ? 'SVf_UTF8' : '0', $pmflags );
-        $initpm->sadd( "RX_EXTFLAGS(PM_GETRE(%s)) = 0x%x;", $sym, $op->reflags );
+        if ( my $pre_saved_sym = $saved_re{$key} ) {
+            $initpm->sadd( "PM_SETRE(%s, ReREFCNT_inc(PM_GETRE(%s)));", $sym, $pre_saved_sym );
+        }
+        else {
+            $initpm->sadd( "PM_SETRE(%s, CALLREGCOMP(newSVpvn_flags(%s, %s, SVs_TEMP|%s), 0x%x));", $sym, $qre, $relen, $utf8 ? 'SVf_UTF8' : '0', $pmflags );
+            $initpm->sadd( "RX_EXTFLAGS(PM_GETRE(%s)) = 0x%x;", $sym, $op->reflags );
+            $saved_re{$key} = $sym;
+        }
 
         if ($eval_seen) {    # set HINT_RE_EVAL off
             $initpm->add('PL_hints = hints_sav;');
