d819d94c11d5a3f0fb15f1536db56efc42b016c6
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2017-08-25 12:06:48 -0600

    Tag B::C as 5.024013_01 for hotfix release

--- lib/B/C.pm
+++ lib/B/C.pm
@@ -11,7 +11,7 @@
 
 package B::C;
 
-our $VERSION = '5.024013';
+our $VERSION = '5.024013_01'; # HF: hotfix release - 66 branch is not tracking bc524 anymore...
 our $caller  = caller;       # So we know how we were invoked.
 
 our @ISA = qw(Exporter);
