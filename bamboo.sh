4f705e00bad424398275cbbbce5f59ff7b6eb241
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2017-08-25 11:33:52 -0600

    cheating using perl for 68

--- bamboo.sh
+++ bamboo.sh
@@ -2,7 +2,9 @@
 
 echo "Removing perl526 & installing perl524"
 [ -e /usr/local/cpanel/3rdparty/perl/526/bin/perl ] && rpm -e --nodeps cpanel-perl-526 ||:
-rpm -Uv --force rpms/cpanel-perl-524-5.24.1-17.cp1162.x86_64.rpm
+#rpm -Uv --force rpms/cpanel-perl-524-5.24.1-17.cp1162.x86_64.rpm
+# cheating for now...
+rpm -Uv --force rpms/cpanel-perl-524-5.24.1-5.cp1168.x86_64.rpm
 
 echo "Setting prove for 524 - can also adjust symlinks"
 [ -L /usr/local/cpanel/3rdparty/bin/prove ] && rm -f /usr/local/cpanel/3rdparty/bin/prove
