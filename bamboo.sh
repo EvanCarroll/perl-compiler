ce165774dff5c6bbbdccb9f79af67bc450cc8526
Todd Rinaldo <toddr@cpanel.net>
2017-10-02 20:21:04 -0500

    Try callregcomp

--- bamboo.sh
+++ bamboo.sh
@@ -2,7 +2,7 @@
 
 echo "Removing perl524 & installing perl526"
 [ -e /usr/local/cpanel/3rdparty/perl/524/bin/perl ] && rpm -e --nodeps cpanel-perl-524 ||:
-rpm -Uv --force rpms/cpanel-perl-526-5.26.0-1.debuginfo.cp1170.x86_64.rpm
+rpm -Uv --force rpms/cpanel-perl-526-5.26.0-2bc.cp1170.x86_64.rpm
 
 echo "Setting prove for 526 - can also adjust symlinks"
 [ -L /usr/local/cpanel/3rdparty/bin/prove ] && rm -f /usr/local/cpanel/3rdparty/bin/prove
